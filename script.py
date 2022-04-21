48e099c9db0bd5579d92ee9b6cc8740d72ec1c10
Rohinimolkere <104123265+Rohinimolkere@users.noreply.github.com>
2022-04-21 11:47:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,4 @@
-print("Print equilateral triangle Pyramid using asterisk symbol ")
-# printing full Triangle pyramid using stars
-size = 7
-m = (2 * size) - 2
-for i in range(0, size):
-    for j in range(0, m):
-        print(end=" ")
-    # decrementing m after each loop
-    m = m - 1
-    for j in range(0, i + 1):
-        print("* ", end=' ')
-    print(" ")
-~                                   
+x=0
+while(x<4):
+    print(x)
+    x=x+1
